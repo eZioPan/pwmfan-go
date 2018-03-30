118591a12280c82d6c93fe6a4afbff6f3b939e8e
eZio Pan <eziopan@qq.com>
2018-03-30 20:58:31 +0800

    redesign data structure

--- dataStructure.go
+++ dataStructure.go
@@ -6,35 +6,43 @@ import (
 	"github.com/stianeikeland/go-rpio"
 )
 
-//Config contain all information that define a PWM fan's user configuration
+// TempPair represent a temperature and correspond Cycle and Count.
+//
+// TempPair struct can be static, as a configuration. Also can be dynamic, as a representation of current fan state.
+type TempPair struct {
+	Temp  float64
+	Cycle uint
+	Count uint
+}
+// NetworkSettings represents a basic network settings
+type NetworkSettings struct {
+	InterfaceName string
+	ListenPort    uint
+	Token         string
+}
+// Config contain all information that define a PWM fan's user configuration
+//
 // TODO: try to implement String() method
 type Config struct {
-	Pin                  uint
-	CPUTempPath          string
-	StartCount           uint
-	StopCount            uint
-	StartTemp            float64
-	LowTemp              float64
-	HighTemp             float64
-	PwmFreq              uint
-	StopCycle            uint
-	StartCycle           uint
-	LowCycle             uint
-	HighCycle            uint
-	FullCycle            uint
-	SampleRate           uint
-	NetworkInterfaceName string
-	ListenPort           int
-	Token                string
+	Pin         uint
+	CPUTempPath string
+	PwmFreq     uint
+	FullCycle   uint
+	SampleRate  uint
+	Start       TempPair
+	Stop        TempPair
+	High        TempPair
+	NetworkSettings
 }
 
-// FanState represent a PWM Fan's running state
+// State represent Fan's current running state
+//
 // TODO: try to implement String() method
-type FanState uint
+type State uint
 
 const (
 	// Stop represent Pwm Fan stay in a full stop state
-	Stop FanState = iota
+	Stop State = iota
 	// Start represent Pwm Fan enter in s Start state
 	Start
 	// Run represent Pwm Fan stay in a running state
@@ -42,33 +50,27 @@ const (
 )
 
 // Fan represent a Fan
-/*
-State, Cycle, Temp should be updated 'in realtime'
-Pin better not be modified after call NewFan()
-*/
+//
+// State, Cycle, Temp should be updated 'in realtime'
+// Pin better not be modified after call NewFan()
+//
 // TODO: try to implement String() method
 type Fan struct {
-	State        FanState
-	Cycle        uint
-	Temp         float64
-	Cfg          Config
-	Pin          rpio.Pin
+	Pin     rpio.Pin
+	Current TempPair
+	StateRecord
+	Cfg     Config
+	UDPAddr *net.UDPAddr
+}
+
+// StateRecord represents a Fan's state and state's counter
+type StateRecord struct {
+	State
 	StopCounter  uint
 	StartCounter uint
-	UDPAddr      *net.UDPAddr
 }
 
-// RemapFunc is a function that read at least one float64 input and output a float64 data
-/*
-More specific,
-this function should take 1st argument as an input data,
-then read more input, and output an data
-Example: LinearRemap and LinearClampRemap
-*/
-
-}
-
-func (state FanState) String() string {
+func (state State) String() string {
 	var str string
 	switch state {
 	case Stop:
@@ -80,4 +82,10 @@ func (state FanState) String() string {
 	}
 	return str
 }
+
+// RemapFunc is a function that read at least one float64 input and output a float64 data
+//
+// More specific,this function should take 1st argument as an input data(s),then read more input, and output result data(s)
+//
+// Example: LinearRemap and LinearClampRemap
 type RemapFunc func([]float64, ...float64) []float64
